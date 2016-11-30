3fd7b94401d4806e52529fa186ae3442fabc1bba
Kevin Truckenmiller <kevintruckenmiller@Kevins-MacBook-Pro.local>
2016-11-30 09:49:35 -0600

    Trying to upgrade to 0.0.9

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+git add .
 
 CURRENT_VERSION=$(git describe --abbrev=0 --tags)
-echo $CURRENT_VERSION
 VERSION=$(echo $CURRENT_VERSION | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
-echo $VERSION
-# docker build -t ktruckenmiller/postgres:$VERSION .
-# docker push ktruckenmiller/postgres:$VERSION
-# git tag $VERSION
-# git push origin --tags
+git commit -m "Trying to upgrade to $VERSION"
+docker build -t ktruckenmiller/postgres:$VERSION .
+docker push ktruckenmiller/postgres:$VERSION
+git push
+git tag $VERSION
+git push origin --tags
