e6652fbdd82dbc590bc5e145e64e06e5f75a6f1d
Kevin Truckenmiller <kevintruckenmiller@Kevins-MacBook-Pro.local>
2016-11-30 10:23:21 -0600

    Trying to upgrade to 0.1.6

--- backup.sh
+++ backup.sh
@@ -3,5 +3,5 @@
 if [ -z "$1" ] &&  [ -z "$2" ]; then
   echo "Make sure you pass args. ./backup.sh user_name db_name file_path"
 else
-  pg_dump --username=$1 --dbname=$2 > $1
+  pg_dump --username=$1 --dbname=$2 > $3
 fi
