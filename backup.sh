cc91314dae4f6c1012fb3fe2f43a7aa3a010dffc
Kevin Truckenmiller <kevintruckenmiller@Kevins-MacBook-Pro.local>
2016-11-30 09:28:42 -0600

    Updated genericicity

--- backup.sh
+++ backup.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-pg_dump --username=indietools --dbname=indietools > /backups/$1
+
+if [ -z "$1" ] &&  [ -z "$2" ]; then
+  echo "Make sure you pass args. ./backup.sh user_name db_name file_path"
+else
+  pg_dump --username=$1 --dbname=$2 > $1
+fi
