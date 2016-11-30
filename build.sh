d3f40ef8c00e5c9a91b2b382e77674b552ab8933
Kevin Truckenmiller <kevintruckenmiller@Kevins-MacBook-Pro.local>
2016-11-30 09:40:24 -0600

    Updated tag

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 CURRENT_VERSION=$(git describe --abbrev=0 --tags)
-
+echo $CURRENT_VERSION
 VERSION=$(echo $CURRENT_VERSION | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
-
-docker build -t ktruckenmiller/postgres:$VERSION .
-docker push ktruckenmiller/postgres:$VERSION
-git tag $VERSION
-git push origin --tags
+echo $VERSION
+# docker build -t ktruckenmiller/postgres:$VERSION .
+# docker push ktruckenmiller/postgres:$VERSION
+# git tag $VERSION
+# git push origin --tags
