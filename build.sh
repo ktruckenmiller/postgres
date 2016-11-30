e6413039a806f76510a74c1d0a58a7f992210a1b
Kevin Truckenmiller <kevintruckenmiller@Kevins-MacBook-Pro.local>
2016-11-30 10:14:43 -0600

    Trying to upgrade to 0.1.5

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ VERSION=$(echo $CURRENT_VERSION | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
 git commit -m "Trying to upgrade to $VERSION"
 docker build -t ktruckenmiller/postgres:$VERSION .
 docker push ktruckenmiller/postgres:$VERSION
+docker tag ktruckenmiller/postgres:$VERSION ktruckenmiller/postgres:latest
+docker push ktruckenmiller/postgres:latest
 git push
 git tag $VERSION
 git push origin --tags
--- restore.sh
+++ restore.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ -z "$1" ] &&  [ -z "$2" ]; then
-  echo "no args given. restore.sh database_name file_name"
+  echo "no args given. `/restore.sh user_name database_name file_name`"
 else
-  psql $1 < $2
+  psql --username=$1 --dbname=$2 < $3
 fi
